fe5d644670ea09171e725b42cf6d7aaa46b12acd
stonefaker1 <70284641+stonefaker1@users.noreply.github.com>
2026-01-09 17:19:19 +0800

    Fix DESCRIPTION variable assignment in blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,8 +16,8 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Yoojay', // 您的昵称 例如 tangly1024
-  DESCRIPTION: process.env.NEXT_PUBLIC_DESCRIPTION || '',
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  DESCRIPTION: process.env.NEXT_PUBLIC_DESCRIPTION || '',
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://yoojay.net', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Yoojay，有界', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
