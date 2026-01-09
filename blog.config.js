d12d96c1f1dfd28c2113e487feb3637d7dd47886
stonefaker1 <70284641+stonefaker1@users.noreply.github.com>
2026-01-09 17:17:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,6 +16,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Yoojay', // 您的昵称 例如 tangly1024
+  DESCRIPTION: process.env.NEXT_PUBLIC_DESCRIPTION || '',
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://yoojay.net', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Yoojay，有界', // 网站关键词 英文逗号隔开
